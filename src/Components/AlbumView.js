a3b5be697c61ea8fc896304f0b807b2310bec85c
SarahWiedrich <sknapcik@gmail.com>
2022-06-23 19:36:37 -0500

    links created and nav buttons added

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -5,10 +5,29 @@ function AlbumView() {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            setAlbumData(data.results)
+        }
+        fetchData()
+    }, [id])
+
+    const songs = albumData.filter(data => data.wrapperType === 'track')
+
+    const display = songs.map(song => {
+        return(
+            <div key={song.trackId}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here</p>
+          {display}
         </div>
     )
 }
--- src/Components/ArtistView.js
+++ src/Components/ArtistView.js
@@ -1,14 +1,51 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+
 
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    const albums = artistData.filter(data => data.collectionType === "Album")
+
+    const display = albums.map((album) => {
+        return (
+            <div key={album.collectionId}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+
+        )
+    })
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            console.log(data)
+            setArtistData(data.results)
+        } 
+        fetchData()
+    },[id])
+
+    const navigate = useNavigate()
+
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    } 
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {navButtons()}
+            {display}
         </div>
     )
 }
